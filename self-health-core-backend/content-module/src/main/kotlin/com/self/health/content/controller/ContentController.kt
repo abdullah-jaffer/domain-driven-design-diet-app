67eb37bf03a218dac1c4963dfac0ae3f66a94887
siddiqueahmad <siddique.ahmad@bazaartech.com>
2024-02-07 15:03:57 +0500

    WIP for testcontainers

--- self-health-core-backend/content-module/src/main/kotlin/com/self/health/content/controller/ContentController.kt
+++ self-health-core-backend/content-module/src/main/kotlin/com/self/health/content/controller/ContentController.kt
@@ -2,14 +2,8 @@ package com.self.health.content.controller
 
 import com.self.health.content.controller.dto.response.PostResponseDto
 import com.self.health.content.service.ContentService
-import com.self.health.user.controller.dto.request.UserCreationRequestDto
-import com.self.health.user.service.UserService
-import org.springframework.http.HttpStatus.CREATED
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 
